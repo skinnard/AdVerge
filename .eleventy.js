1b9f45fd66b75137724cba12ecd8fa487cd8cdd1
Glen Hodges <glenrhodges@gmail.com>
2023-12-15 12:40:12 -0500

    ADded favicon

--- .eleventy.js
+++ .eleventy.js
@@ -7,44 +7,45 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(pluginRss);
 
   // shortcode to render markdown from string => {{ STRING | markdown | safe }}
-  eleventyConfig.addFilter('markdown', function(value) {
-    let markdown = require('markdown-it')({
-      html: true
+  eleventyConfig.addFilter("markdown", function (value) {
+    let markdown = require("markdown-it")({
+      html: true,
     });
     return markdown.render(value);
   });
 
   // rebuild on CSS changes
-  eleventyConfig.addWatchTarget('./src/_includes/css/');
+  eleventyConfig.addWatchTarget("./src/_includes/css/");
 
   // Markdown
   eleventyConfig.setLibrary(
-    'md',
+    "md",
     markdownIt({
       html: true,
       breaks: true,
       linkify: true,
-      typographer: true
+      typographer: true,
     })
-  )
+  );
 
-  
   //create collections
-  eleventyConfig.addCollection('sections', async (collection) => {
-    return collection.getFilteredByGlob('./src/sections/*.md');
+  eleventyConfig.addCollection("sections", async (collection) => {
+    return collection.getFilteredByGlob("./src/sections/*.md");
   });
-  
+
   // STATIC FILES
-  eleventyConfig.addPassthroughCopy({ './src/static/': '/' });
-  eleventyConfig.addPassthroughCopy("bundle.js");
+  eleventyConfig.addPassthroughCopy({ "./src/static/": "/" });
+
+  // Copy `img/favicon/` to `_site/`
+  // eleventyConfig.addPassthroughCopy({ "img/favicon": "/" });
 
   // TRANSFORM -- Minify HTML Output
-  eleventyConfig.addTransform("htmlmin", function(content, outputPath) {
-    if( outputPath && outputPath.endsWith(".html") ) {
+  eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
+    if (outputPath && outputPath.endsWith(".html")) {
       let minified = htmlmin.minify(content, {
         useShortDoctype: true,
         removeComments: true,
-        collapseWhitespace: true
+        collapseWhitespace: true,
       });
       return minified;
     }
@@ -53,17 +54,13 @@ module.exports = function (eleventyConfig) {
 
   return {
     dir: {
-      input: 'src',
-      output: 'public',
-      data: './_data',
-      includes: './_includes',
-      layouts: './_layouts'
+      input: "src",
+      output: "public",
+      data: "./_data",
+      includes: "./_includes",
+      layouts: "./_layouts",
     },
-    templateFormats: [
-      'md',
-      'njk',
-      '11ty.js'
-    ],
-    htmlTemplateEngine: 'njk'
+    templateFormats: ["md", "njk", "11ty.js"],
+    htmlTemplateEngine: "njk",
   };
 };
